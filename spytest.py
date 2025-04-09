5d1d834c16b01d92027b1458498ea50d8fba4c59
amor$code <148223385+Ambrosius03@users.noreply.github.com>
2025-04-09 15:13:52 +0700

    Update spytest.py

--- spytest.py
+++ spytest.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
 
 # Load data
-data = pd.read_excel('data-perceraian.xlsx')
-if data.isnull().values.any():
-    st.warning('Data mengandung nilai kosong. Harap periksa kembali dataset Anda.')
-
+data = pd.read_csv('data-perceraian.csv', sep=';')
 
 # Judul Dashboard
 st.title('Dashboard Data Perceraian di Indonesia')
@@ -21,23 +19,18 @@ st.write(data.describe())
 # Pilih Provinsi
 provinsi = st.selectbox('Pilih Provinsi:', data['nama_provinsi'].unique())
 data_filtered = data[data['nama_provinsi'] == provinsi]
-if data_filtered.empty:
-    st.warning(f'Tidak ada data untuk provinsi {provinsi}.')
-else:
-    # Lanjutkan dengan visualisasi
-
 
 # Visualisasi jumlah perceraian per tahun
- st.subheader(f'Jumlah Perceraian di {provinsi} per Tahun')
- fig, ax = plt.subplots()
- data_filtered.groupby('tahun')['jumlah_perceraian'].sum().plot(kind='bar', ax=ax)
- plt.ylabel('Jumlah Perceraian')
- plt.xlabel('Tahun')
- st.pyplot(fig)
+st.subheader(f'Jumlah Perceraian di {provinsi} per Tahun')
+fig, ax = plt.subplots()
+data_filtered.groupby('tahun')['jumlah_perceraian'].sum().plot(kind='bar', ax=ax)
+plt.ylabel('Jumlah Perceraian')
+plt.xlabel('Tahun')
+st.pyplot(fig)
 
 # Visualisasi penyebab perceraian
- st.subheader(f'Penyebab Perceraian di {provinsi}')
- fig2, ax2 = plt.subplots()
- data_filtered['faktor_penyebab'].value_counts().plot(kind='barh', ax=ax2)
- plt.xlabel('Jumlah Kasus')
- st.pyplot(fig2)
+st.subheader(f'Penyebab Perceraian di {provinsi}')
+fig2, ax2 = plt.subplots()
+data_filtered['faktor_penyebab'].value_counts().plot(kind='barh', ax=ax2)
+plt.xlabel('Jumlah Kasus')
+st.pyplot(fig2)
